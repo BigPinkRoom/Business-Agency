9f29ee8ce362eec925e8d2151dd0e773fbfbfcc2
tester2020 <tester2020@tester2020.com>
2020-08-23 16:13:47 +0300

    fixed double quotes, fixed small promlems

---
+++ js/app.js
@@ -0,0 +1,74 @@
+(function(){
+
+    // Constants
+    const header = document.getElementById('header');
+    const navToggle = document.getElementById('nav-toggle');
+    const navLink = document.querySelector('.nav__link')
+    const intro = document.getElementById('intro');
+    
+    // Burger menu toggle
+    function toggleHeaderBurger() {
+        header.classList.toggle('active');
+    }
+
+    function hideHeaderActiveBurger() {
+        if (event.target.closest('.nav__link') && event.target.closest('.header.active')) header.classList.toggle('active');
+        return
+    }
+
+    navToggle.addEventListener('click', toggleHeaderBurger);
+    document.addEventListener('click', hideHeaderActiveBurger);
+
+    // Fix header on scroll
+    function toggleFixedHeader() {
+        let scrollHeight = window.pageYOffset,
+            introHeight = intro.offsetHeight - header.offsetHeight;
+
+        if(scrollHeight >= introHeight && !header.classList.contains('fixed')) {
+            header.classList.add('fixed');
+        }
+
+        if(scrollHeight < introHeight && header.classList.contains('fixed')) {
+            header.classList.remove('fixed');
+        }
+    }
+
+    document.addEventListener('scroll', toggleFixedHeader);
+    toggleFixedHeader();
+
+    // Smooth scroll
+    function smoothScroll() {
+        if(!event.target.dataset.scroll) return
+        if(!document.getElementById(event.target.dataset.scroll)) {
+            document.location.href = `index.html#${event.target.dataset.scroll}`;
+        }
+        event.preventDefault();
+
+        let element = document.getElementById(event.target.dataset.scroll),
+            elementStartPosition = element.getBoundingClientRect().top + window.scrollY - header.offsetHeight;
+            startPosition = window.pageYOffset,
+            distance = elementStartPosition - startPosition,
+            duration = 1000,
+            startTime = null;
+
+        function animation(currentTime) {
+            if(!startTime) startTime = currentTime;
+            let timeElapsed = currentTime - startTime;
+            let run = ease(timeElapsed, startPosition, distance, duration);
+            window.scrollTo(0, run);
+            if(timeElapsed < duration) requestAnimationFrame(animation);
+        }
+
+        function ease(timeElapsed, startPosition, distance, duration) {
+           timeElapsed /= duration / 2;
+           if (timeElapsed < 1) return distance / 2 * timeElapsed * timeElapsed * timeElapsed + startPosition;
+           timeElapsed--;
+           return -distance / 2 * (timeElapsed * (timeElapsed - 2) - 1) + startPosition;
+        }
+
+        requestAnimationFrame(animation)
+    }
+
+    document.addEventListener('click', smoothScroll)
+
+})()
